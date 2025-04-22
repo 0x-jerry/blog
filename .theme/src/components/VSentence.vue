6fab72c70259b78b94091f1b57811849ae13c8a3
Jerry <x.jerry.wang@gmail.com>
2025-04-22 03:42:32 +0000

    perf: tweak style

--- .theme/src/components/VSentence.vue
+++ .theme/src/components/VSentence.vue
@@ -16,16 +16,18 @@ const from = computed(() => {
 
   return [data.from, data.from_who].filter(Boolean).join(' - ')
 })
-
-fetchHitokoto()
 </script>
 
 <template>
   <div class="text-lg">
     <div class="v-sentence text-center px-4">
-      <template v-if="!hitokoto.isLoading">
-        <!-- todo, add loading -->
-        <div class="loading"></div>
+      <template v-if="hitokoto.isLoading.value">
+        <div class="flex justify-center py-4 gap-2">
+          <div>Loading</div>
+          <div class="flex gap-1 justify-center relative top-1">
+            <span class="dot" v-for="_ in 3">•️</span>
+          </div>
+        </div>
       </template>
       <template v-else>
         <p class="font-italic">「 {{ hitokoto.data.value?.hitokoto }} 」</p>
@@ -35,4 +37,20 @@ fetchHitokoto()
   </div>
 </template>
 
-<style></style>
+<style lang="less" scoped>
+.dot {
+  @apply block animate-bounce;
+
+  &:nth-child(1n) {
+    animation-delay: 0ms;
+  }
+
+  &:nth-child(2n) {
+    animation-delay: 100ms;
+  }
+
+  &:nth-child(3n) {
+    animation-delay: 200ms;
+  }
+}
+</style>
