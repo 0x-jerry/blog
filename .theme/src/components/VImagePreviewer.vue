6c23355b8ad6ce6554788631e47ef7060dbb67a5
Jerry <x.jerry.wang@gmail.com>
2025-11-05 23:31:00 +0800

    feat: VImagePreviewer component

---
+++ .theme/src/components/VImagePreviewer.vue
@@ -0,0 +1,58 @@
+<script lang='ts' setup>
+import '../lib/img-previewer/index.less'
+import VIcon from './VIcon.vue';
+
+const props = defineProps<{
+  selector: string
+}>()
+
+
+function handleClick(e: MouseEvent) {
+  // 
+
+}
+
+</script>
+
+<template>
+  <div class="v-image-previewer" @click="handleClick">
+    <slot></slot>
+  </div>
+
+  <div class="img-pre__container">
+    <div class="img-pre__header img-pre__animated">
+      <div class="img-pre__nums">
+        <p>
+          <span id="img-pre__current-index"></span>
+          <span class="img-pre__nums-delimiter">/</span>
+          <span id="img-pre__total-index"></span>
+        </p>
+      </div>
+      <div class="img-pre__buttons" id="J_header-buttons">
+        <button class="img-pre__button-item" data-action="reset">
+          <VIcon class="i-carbon:reset" />
+        </button>
+        <button class="img-pre__button-item" data-action="rotateLeft">
+          <VIcon class="i-carbon:rotate-counterclockwise" />
+        </button>
+        <button class="img-pre__button-item" data-action="rotateRight">
+          <VIcon class="i-carbon:rotate-clockwise" />
+        </button>
+        <button class="img-pre__button-item" data-action="close">
+          <VIcon class="i-carbon:close-large" />
+        </button>
+      </div>
+    </div>
+    <div class="img-pre__content-wrapper" id="J_content-wrapper">
+      <div class="img-pre__img-item img-pre__animated current-index" id="J_current-index">
+        <img alt="" id="img-pre__image" src="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang='less'>
+.v-image-previewer {
+  display: contents;
+}
+</style>
--- .theme/src/pages/Post.vue
+++ .theme/src/pages/Post.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import VArticleNav from '@@/components/VArticleNav.vue'
 import VGiscus from '@@/components/VGiscus.vue'
+import VImagePreview from '@@/components/VImagePreviewer.vue'
 import VTitle from '@@/components/VTitle.vue'
 import LayoutRightSlot from '@@/layout/components/LayoutRightSlot.vue'
 import { useRoute } from 'vitepress'
@@ -19,31 +20,33 @@ const headers = computed(() => route.data.headers)
   <div class="v-post">
     <VTitle :title="route.data.title" show-back></VTitle>
 
-    <div class="flex mt-4">
-      <div class="post-content flex-1 w-0 px-4 xl:px-2">
-        <div class="heti text-left max-w-full" ref="content">
-          <Content></Content>
+    <VImagePreview selector="img">
+      <div class="flex mt-4">
+        <div class="post-content flex-1 w-0 px-4 xl:px-2">
+          <div class="heti text-left max-w-full" ref="content">
+            <Content></Content>
 
-          <div class="pt-6">
-            <div class="text-center c-bPrimary-2 text-6xl relative">
-              <span> 完 </span>
-              <span
-                class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 inline-block size-1.8em border-(2 dashed bPrimary-1) rounded-full"
-              />
+            <div class="pt-6">
+              <div class="text-center c-bPrimary-2 text-6xl relative">
+                <span> 完 </span>
+                <span
+                  class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 inline-block size-1.8em border-(2 dashed bPrimary-1) rounded-full" />
+              </div>
             </div>
           </div>
+
+          <br />
+
+          <ClientOnly>
+            <VGiscus class="mt-8" v-if="enableComment" />
+          </ClientOnly>
         </div>
-
-        <br />
-
-        <ClientOnly>
-          <VGiscus class="mt-8" v-if="enableComment" />
-        </ClientOnly>
       </div>
-      <LayoutRightSlot v-if="headers.length">
-        <VArticleNav class="post-nav" :headers="headers" />
-      </LayoutRightSlot>
-    </div>
+    </VImagePreview>
+
+    <LayoutRightSlot v-if="headers.length">
+      <VArticleNav class="post-nav" :headers="headers" />
+    </LayoutRightSlot>
   </div>
 </template>
 
